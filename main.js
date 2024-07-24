dcebd0b23fa7dd8c9a5453166cd1667be79094ab
akri <alecjoseph.dev@gmail.com>
2024-07-24 16:29:39 +0800

    added NPM tutorial

---
+++ main.js
@@ -0,0 +1,14 @@
+// var myDate = new Date();
+// console.log(myDate);
+
+var moment = require('moment');
+var unique = require('uniq');
+
+var myDate = new Date();
+var myCoolDate = moment(myDate).format('LL');
+
+var myList = [1,2,1,1,1,3,3,4,5,6,4,4,7];
+var myUniqueList = unique(myList);
+
+console.log(myCoolDate);
+console.log(myUniqueList);
